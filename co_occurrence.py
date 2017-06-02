a6cef3e0be9ec82c0f35386d89e5a057c1e04f32
Jim Brunner <jimbrunner@Jims-MacBook-Pro.local>
2017-06-02 14:33:49 -0600

    Added Colors, removed rows of 0, added relative binning

--- co_occurrence.py
+++ co_occurrence.py
@@ -12,7 +12,6 @@
 from pylab import *
 import pandas as pd
 import sys
-from datetime import datetime
 
 #####Variables
 ##User input, should be entered as an argument
@@ -38,6 +37,22 @@ def both_same_bin(r1,r2, threshes = [0.333,0.666]):
 	tot_same_bin = sum(both_in_bin)
 	same_bin_frac = float(tot_same_bin)/float(len(r1))
 	return same_bin_frac
+	
+#Function that counts the number of times the two occur in the same abundance range relative
+#to the maximum level it appears in samples (in which it appears)
+def both_same_rel_bin(r1,r2, threshes = [0.1,0.333,0.666]):
+# 	if threshes[0] != 0:
+# 		pad(threshes,(1,0),'constant')
+	k = len(threshes)
+	r1m = max(r1)
+	r2m = max(r2)
+	r1rel = r1/r1m
+	r2rel = r2/r2m	
+	up_bds = append(threshes[1:],1)
+	both_in_bin = [both_occ(r1rel,r2rel,lbd = threshes[i],ubd = up_bds[i]) for i in range(k)]
+	tot_same_bin = sum(both_in_bin)
+	same_bin_frac = float(tot_same_bin)/float(len(r1))
+	return same_bin_frac
 
 #Function that classifies nodes by which type of sample they have the highest abundance in.
 #I need to consider how to deal with nodes that appear at high levels in more than one 
@@ -48,39 +63,57 @@ def color_picker(r):
 	strgth = r[most_found] - mean(r)
 	std_dev = std(r)
 	if strgth >= std_dev:
-		return most_found
+		colors = linspace(0,255,numcols)
+		numwhere = where(r.index == argmax(r))
+		color_picked = colors[numwhere]
+		rgb_value = cm.rainbow(color_picked.astype(int))
+		hex_value = matplotlib.colors.rgb2hex(rgb_value[0])
+		return [most_found, hex_value]
 	else:
-		return 'Mixed'
+		grey = matplotlib.colors.rgb2hex([0.80000001192092896, 0.80000001192092896, 0.80000001192092896, 1.0])
+		return ['Mixed', grey]
 
 def matchyn(a,b):
 	if a==b:
 		return a
 	else:
-		return 'Intertype'
+		grey = matplotlib.colors.rgb2hex([0.80000001192092896, 0.80000001192092896, 0.80000001192092896, 1.0])
+		return ['Intertype', grey]
 
 ######Import the abundance matrix
 #Makes a pandas DataFrame, use abundance_array.keys() to see column labels, 
-abundance_array = pd.read_csv(csv_name, sep = ' ')[:50]
-num_genomes = len(abundance_array)
+abundance_array = pd.read_csv(csv_name, sep = ' ')
+#print abundance_array
+#Let's remove taxa that aren't present at all.
+for ind in abundance_array.index:
+	if max(abundance_array.loc[ind][2:]) == 0:
+		abundance_array = abundance_array.drop(ind)
+
+abundance_array.index = range(len(abundance_array))		
 
 #We want to make a network at a specific level, or set of levels
 if level == 'all':
 	level = array(abundance_array['LEVEL'].values)
 	level = array(unique(level))
+	print level
 else:
-	level = array(level)
+	level = array([level])
 
 #need to check that the levels given as argument actually exist
 levels_allowed = array(abundance_array['LEVEL'].values)
+levels_allowed = array(unique(levels_allowed))
+
 for i in level:
 	if not i in levels_allowed:
-		sys.exit('Bad level argument. Choose a level or type all')
+		print levels_allowed
+		sys.exit('Bad level argument. Choose a level from the list above or type all')
 
 #Combine samples of the same kind (that is, the same part of the body) so that we can 
 #color according to where abundance of a genome is highest.
 diff_samps_types = unique([name[:-10] for name in array(abundance_array.keys())[2:]])
 data_samps_types = transpose([sum([abundance_array[samp] for samp in abundance_array.keys()[2:]
 							 if smp_type in samp],axis = 0) for smp_type in diff_samps_types])
+
 samp_type_abund = pd.DataFrame(data_samps_types, columns = diff_samps_types)
 samp_type_abund['TAXA'] = abundance_array['TAXA']
 
@@ -99,10 +132,12 @@ for i in level:
 
 	#Create numpy array of co-occurrence fractions. This is the incidence matrix for our weighted
 	#graph.
-	adj_matrix = asarray([[0 if x == y else both_same_bin(ab_np_array[x],ab_np_array[y],threshes = [0.1,0.3,0.5,0.7,0.9]) 
+# 	adj_matrix = asarray([[0 if x == y else both_same_bin(ab_np_array[x],ab_np_array[y],threshes = [0.05,0.5]) 
+# 							for x in range(len(ab_np_array))] for y in range(len(ab_np_array))])
+#     adj_matrix = asarray([[0 if x == y else both_occ(ab_np_array[x],ab_np_array[y], lbd = 0.1) 
+# 							for x in range(len(ab_np_array))] for y in range(len(ab_np_array))])
+	adj_matrix = asarray([[0 if x == y else both_same_rel_bin(ab_np_array[x],ab_np_array[y]) 
 								for x in range(len(ab_np_array))] for y in range(len(ab_np_array))])
-#   adj_matrix = asarray([[0 if x == y else both_occ(ab_np_array[x],ab_np_array[y], lbd = 0.1) 
-#								for x in range(len(ab_np_array))] for y in range(len(ab_np_array))])
 								
 	in_level = in_level[0]
 	adjacency_frames[i] = pd.DataFrame(adj_matrix, index = abundance_array['TAXA'][in_level], columns = abundance_array['TAXA'][in_level])
@@ -111,20 +146,25 @@ for i in level:
 	#create a list of edges - source in one column, target in the other. This is an alternative to the adjacency matrix
 	#that might make it easier to load in to cytoscape.
 	num_edge = count_nonzero(adj_matrix)
-	print num_edge
+	print i
+	print num_edge/2
 	source_target_data = []#empty([num_edge,3], dtype = 'string')	
 	for l in range(len(adj_matrix)):
 		for k in range(l+1):
 				if adj_matrix[l,k] != 0:
 					s_type1 = color_picker(samp_type_abund.iloc[in_level[l]][:-1])
 					s_type2 = color_picker(samp_type_abund.iloc[in_level[k]][:-1])
-					edge =  [abundance_array['TAXA'][in_level[l]],str(adj_matrix[l,k]),
-							abundance_array['TAXA'][in_level[k]],s_type1,s_type2, matchyn(s_type1,s_type2)]
+					edge_samp = matchyn(s_type1,s_type2)
+					edge =  [abundance_array['TAXA'][in_level[l]], abundance_array['TAXA'][in_level[k]],
+												str(adj_matrix[l,k]),s_type1[0],s_type2[0],edge_samp[0],s_type1[1],s_type2[1],edge_samp[1]]
+					rev_edge = [abundance_array['TAXA'][in_level[k]], abundance_array['TAXA'][in_level[l]],
+												str(adj_matrix[l,k]),s_type2[0],s_type1[0],edge_samp[0],s_type2[1],s_type1[1],edge_samp[1]]
 					source_target_data += [edge]
-#					source_target_data += [list(reversed(edge))]
+					source_target_data += [rev_edge]
 				
 	#turn list into a dataframe
-	source_target_frames[i] = pd.DataFrame(source_target_data, columns = ['source','strength','target','first_sample','second_sample','edge_sample'])
+	source_target_frames[i] = pd.DataFrame(source_target_data, columns = ['source','target','weight',
+						'first_sample','second_sample','edge_sample','fscolor','sscolor','edcolor'])
 	
 	#Assign a color to each node based on where they are most abundant (what sample type)
 
@@ -133,14 +173,13 @@ for i in level:
 #cytoscape and provides a way to include edge or node attributes.
 for i in level:
 #	adjacency_frames[i].to_csv(net_name+i+'_adj.mat', sep = '\t')
-	source_target_frames[i].to_csv(net_name+i+'_list.tsv', sep = '\t')
+	source_target_frames[i].to_csv(net_name+'_'+i+'_list.tsv', sep = '\t')
 
 
 
 #### TO DO #####################
 #
-# - Classify each node in the network according to what type of sample it appears in
-#		~ decide how to classify nodes that appear in multiple types of sample at 
+# - Decide how to classify nodes that appear in multiple types of sample at 
 #				high frequency
 #
 # - Create separate networks for each sample type
